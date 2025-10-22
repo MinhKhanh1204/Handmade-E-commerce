aed34322ae1a70c066a9f8762e769196823fdf11
MinhTran7 <he187170tranminhlam@gmail.com>
2025-10-23 00:15:41 +0700

    Apply .gitignore

---
+++ HandicraftShop_Prodject/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandicraftShop_Prodject.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
