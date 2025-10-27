080024c0aae9d71caf69aa39f8c47114cedbab28
HuyLu003 <HuyLVQCE180656@fpt.edu.vn>
2025-10-27 20:30:34 +0700

    them chuc nang quan ly Staff va Product

---
+++ Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BussinessObject;
+using System.Collections.Generic;
+
+namespace Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetCategories();
+        Category? GetCategoryById(int id);
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int id);
+    }
+}
---
+++ Repositories/StaffRepository .cs
@@ -0,0 +1,50 @@
+﻿using BussinessObject;
+using DataAccessObject;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repositories
+{
+    public class StaffRepository : IStaffRepository
+    {
+        private readonly MyStoreContext _context;
+
+        public StaffRepository(MyStoreContext context)
+        {
+            _context = context;
+        }
+
+        public List<Staff> GetAll()
+        {
+            return _context.Staffs
+                .Include(s => s.StaffNavigation)
+                .ToList();
+        }
+
+        public Staff? GetById(string staffId)
+        {
+            return _context.Staffs
+                .Include(s => s.StaffNavigation)
+                .FirstOrDefault(s => s.StaffId == staffId);
+        }
+
+        public void Add(Staff staff)
+        {
+            _context.Staffs.Add(staff);
+            _context.SaveChanges();
+        }
+
+        public void Update(Staff staff)
+        {
+            _context.Staffs.Update(staff);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Staff staff)
+        {
+            _context.Staffs.Remove(staff);
+            _context.SaveChanges();
+        }
+    }
+}
