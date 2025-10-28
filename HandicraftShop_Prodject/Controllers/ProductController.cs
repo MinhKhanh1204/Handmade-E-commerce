a5b9914043bf190279036f5ddca331ab4abc158f
minhpthe180267 <minhpthe180267@fpt.edu.vn>
2025-10-29 02:28:37 +0700

    feat: add UI Product Page

---
+++ HandicraftShop_Prodject/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandicraftShop_Prodject.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
